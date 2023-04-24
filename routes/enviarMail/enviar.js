dd2e144e305463ad724f811e0d304519eacd2596
Amandalfs <caduc57@gmail.com>
2023-04-24 19:14:34 -0300

    feat
    Criado a rota para rascunho para enviar um email

---
+++ routes/enviarMail/enviar.js
@@ -0,0 +1,58 @@
+const sgMail = require('@sendgrid/mail')
+const { config } = require('dotenv');
+config()
+
+const enviarEmail = (app)=>{
+    app.route('/send')
+        .post((req, res)=>{
+            const {texto, emailReceive, titulo} = req.body;
+
+            const errors = [];
+
+            if(!texto){
+                errors.push("Campo texto vazio")
+            } 
+
+            if(!emailReceive){
+                errors.push("Email vazio");
+            }
+
+            if(!emailReceive){
+                errors.push("Titulo Vazio");
+            }
+
+            if(errors.length!==0){
+                res.status(400).send({errors})
+                return
+            }
+
+            try {   
+                const msg = {
+                    to: emailReceive, // Change to your recipient
+                    from: process.env.EmailSend, // Change to your verified sender
+                    subject: titulo,
+                    text: texto,
+                    html: `<p>${texto}</p>`,
+                }
+    
+                sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+                sgMail
+                .send(msg)
+                    .then(() => {
+                        console.log('Email send')
+                        res.status(201).send("Email Enviado")
+                        return
+                    })
+                    .catch((error) => {
+                        console.error(error)
+                        res.send({error})
+                })
+
+            } catch (error) {
+                console.log(error)
+            }
+           
+        })
+}
+
+module.exports = enviarEmail
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const cors = require('cors')
+const enviarEmail = require('./routes/enviarMail/enviar')
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+//rotas
+enviarEmail(app);
+
 
 app.listen(8020, ()=>{
     console.log("Servidor Ativo")
